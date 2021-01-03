0ce628c4a5781d7861d3a803ecdd0a670663dc65
Royce <royce.com@gmail.com>
2021-01-03 09:04:00 +0800

    Initial Commit

---
+++ Todo List/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Todo List
+//
+//  Created by Royce Remulla on 1/3/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
